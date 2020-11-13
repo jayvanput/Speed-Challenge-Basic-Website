8c2dcdebadc81b33fd15a5028c09b81483c0ef51
jayvanput <jayvanput@gmail.com>
2020-11-13 00:41:14 -0500

    set up html divs and messed around with fetching API

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-document.getElementById("test").innerText = "hello world"
+async function getData() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/1/');
+    const data = await response.json();
+    document.getElementById('head__name').innerText = data.name;
+}
+
+getData()
