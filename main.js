7b8c2606641f3476d11627170b2bd92635dc07a4
jayvanput <jayvanput@gmail.com>
2020-11-13 01:42:29 -0500

    Implemented API interaction with pokemon IDs.

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-async function getData() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/1/');
+async function getData(number) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`);
     const data = await response.json();
-    document.getElementById('head__name').innerText = data.name;
+    document.getElementById150150('head__name').innerText = data.name;
+    document.getElementById('info__img').src = data.sprites.front_default;
 }
 
-getData()
+document.getElementById('test').addEventListener("click", function () {
+    let num = document.getElementById('test-input').value
+    getData(num);
+});
