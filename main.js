b0dab62d112376c051895975119a4c47d54c5474
jayvanput <jayvanput@gmail.com>
2020-11-14 17:09:22 -0500

    mimic a PHP server to push to Heroku?

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 async function getData(number) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`);
+    if (number < 1 || number > 893) {
+        number = 1
+    }
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`)
+        .then((response) => {
+            if (!response.ok) {
+                alert('did not work')
+            }
+            return response
+        });
+
     const data = await response.json();
-    document.getElementById150150('head__name').innerText = data.name;
+    document.getElementById('head__name').innerText = data.name;
     document.getElementById('info__img').src = data.sprites.front_default;
 }
 
 document.getElementById('test').addEventListener("click", function () {
     let num = document.getElementById('test-input').value
-    getData(num);
+    getData(num)
+        .catch(error => {
+            console.error(error)
+        });
 });
